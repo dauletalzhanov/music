ba6002b0311db3338d0110a4555dcf3ab9e9ae13
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-04-13 17:37:02 +0100

    added album section

--- src/Pages/Album.jsx
+++ src/Pages/Album.jsx
@@ -4,50 +4,71 @@ import React from "react"
 
 import Header from "../Components/Header"
 
+import "./CSS/album.css"
+
 export default function Album(){
     const params =  useParams()
     const albumID = params["id"]
-    //const [albumInfo, setAlbumInfo] = useState({})
-	let tracks = []
-	let albumInfo = {}
+
+    const [trackList, setTracks] = useState([])
+	const [albumInfo, setAlbumInfo] = useState({})
 
     useEffect(()=>{
-        let isMounted = true;
-
         async function getAlbums(){
             let url = `https://itunes.apple.com/lookup?id=${albumID}&entity=song`
             let results = await fetch(url, { mode: 'cors' })
             results = await results.json()
+            results = results.results
 
-            if (isMounted) {
-				albumInfo = {
-					...results.results.slice(0)[0]
-				}
-                tracks = [
-					...results.results.slice(1)
-				]
-
-				console.log(tracks)
-                console.log(albumInfo)
-            }
+            let albums = results.slice(0)[0]
+            albums.releaseDate = albums.releaseDate.slice(0, 10)
             
-			document.title = `Album: ${albumInfo.collectionName}`
+            setTracks(results.slice(1))
+            setAlbumInfo(albums)
+            
+            //console.log(albumInfo)
+
+            
+            document.title = "Album: " + albumInfo.collectionName
         }
         getAlbums();
-
-        return () => {
-            isMounted = false;
-        };
-
-    }, [])
-
-	//let nuke = Object.keys(albumInfo).map(k => (<p key={k}>{albumInfo[k]}</p>))
+    }, [trackList, albumInfo])
 
     return(<>
         <Header></Header>
-        <h1>{albumID}</h1>
-        
-        
+        <div id="album-details">
+            <div className="album-leftie">
+                <div>
+                    <div className="album-name">
+                        <img src={albumInfo.artworkUrl100} alt="album cover" id="album-cover" />
+                        <div>
+                            <h2>{albumInfo.collectionName}</h2>
+                            <h2>{albumInfo.artistName}</h2>
+                        </div>
+
+                    </div>
+                    <div className="album-metrics">
+                        <p>Genre: <strong>{albumInfo.primaryGenreName}</strong></p>
+                        <p>Tracks: <strong>{albumInfo.trackCount}</strong></p>
+                        <p>Country: <strong>{albumInfo.country}</strong></p>
+                        <p>Release: <strong>{albumInfo.releaseDate}</strong></p>
+                    </div>
+                </div>
+                <div className="gray">
+                    <p>{albumInfo.copyright}</p>
+                    <a href={albumInfo.collectionViewUrl}>iTunes Link</a>
+                </div>
+            </div>
+            <img src="" alt="Music Video of 'Artist Name - Song Name'" />
+        </div>
+
+        <div id="trackList">
+            { trackList.map(i => {
+                let content = `${i.trackNumber}. `
+                content += `${i.trackName}`
+                return <p key={i.trackNumber}> {content} </p>
+            })}
+        </div>
     
     </>)
 }
