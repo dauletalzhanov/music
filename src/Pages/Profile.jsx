e99833ede851791677b89afe4027d7bee552f2b0
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-04-14 11:46:16 +0100

    able to browse playlist songs from firebase

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,5 +1,9 @@
 import Header from "../Components/Header"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from '../../firebase'
 
 import "./CSS/profile.css"
 
@@ -9,26 +13,48 @@ export default function Profile(){
 	let url = `https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${query}`
 	let [embedURL, setEmbedURL] = useState('')
 	let [alt_text, setAltText] = useState('')
+	let [playlist, setPlaylist] = useState([])
 	
 	useEffect(() => {	
 		document.title = `Profile`
-		
 
 		async function getGIF(){
 			let result = await fetch(url, { mode: 'cors' })
 			result = await result.json()
 			setEmbedURL(result.data.images.downsized_medium.url)
 			setAltText()
-	
-			console.log(result)
 		}
 
 		getGIF()
 	}, [url])
 
+	useEffect(()=>{
+		async function getPlaylist(db){
+			const playlistColl = collection(db, "playlist")
+			const playlistDocs = await getDocs(playlistColl)
+			const playlistSnap = playlistDocs.docs.map(doc => doc.data())
+
+			setPlaylist(playlistSnap)
+		  
+			console.log(playlistSnap)
+		  
+		  }
+		  
+		getPlaylist(db)
+
+
+	}, [])
+
 	function updateQuery(event){
 		setQuery(event.target.value)
 		url = `https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${query}`
+	}
+
+	function removeSong(event){
+		const songName = event.target.querySelector(".songName").innerHTML
+		const artistName = event.target
+
+		console.log(`removing ${songName}`)
 	}
 
 	return(<>
@@ -48,7 +74,7 @@ export default function Profile(){
 				<div className="metrics">
 					<div className="stat">
 						<p>Number of Songs:</p>
-						<p><strong>{16}</strong></p>
+						<p><strong>{ playlist.length }</strong></p>
 					</div>
 					<div className="stat">
 						<p>Favourite Genre:</p>
@@ -69,21 +95,24 @@ export default function Profile(){
 		<div className="playlist">
 			<div className="playlist-title">
 				<h3>SAVED SONGS</h3>
-				<h3>TOTAL SONGS: {16}</h3>
+				<h3>TOTAL SONGS: { playlist.length }</h3>
 			</div>
 
 			<div className="playlist-content">
-				<div className="playlist-song">
-					<div className="leftie">
-						<div className="album-cover"></div>
-						<div className="song-details">
-							<p>{"Dua Lipa"}</p>
-							<p>{"Dance the Night"}</p>
-							<p>{"4:33"}</p>
+				{ playlist.map((song, index) => {
+					return (<div key={index} className="playlist-song">
+						<div className="leftie">
+							<img src={song.artworkURL} alt={"album cover for " + song.artistName + song.songName} className="album-cover" />
+							<div className="song-details">
+								<p className="artistName"> <Link to={song.artistURL}> {song.artistName}</Link></p>
+								<p className="songName">{song.songName}</p>
+								<p>{song.length}</p>
+							</div>
+
 						</div>
-					</div>
-					<button id="remove-button">REMOVE</button>
-				</div>
+						<button id="remove-button" onClick={removeSong}>REMOVE</button>
+					</div>)
+				})}
 			</div>
 		</div>
 	</>)
