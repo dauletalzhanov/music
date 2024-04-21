11e515fb3756e4a6fba608d1f4b020ba01b50532
dauletalzhanov <35165187+dauletalzhanov@users.noreply.github.com>
2024-04-21 02:30:06 +0100

    hiding contents of the .env files in production mode

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, doc, collectionGroup, addDoc, getDocs, where, query, orderBy } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_firebase_key,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey: import.meta.env.VITE_FIREBASE_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig);
